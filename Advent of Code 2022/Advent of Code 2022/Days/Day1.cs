b73d85d795fcecf8bfe60f51a4a7b492aa423bdc
wvm28 <wouter.van.mierlo@sogeti.com>
2022-12-01 11:37:05 +0100

    completed day1 of advent of code

---
+++ Advent of Code 2022/Advent of Code 2022/Days/Day1.cs
@@ -0,0 +1,67 @@
+﻿namespace Advent_of_Code_2022.Days
+{
+    class Day1
+    {
+        private string _inputFilePath = Path.Combine(Environment.CurrentDirectory + @"\..\..\..\Days\Input\Day1.txt");
+        private List<int> _calories = new List<int>();
+        public Day1()
+        {
+            foreach (string line in System.IO.File.ReadLines(_inputFilePath))
+            {
+                if (String.IsNullOrEmpty(line))
+                {
+                    _calories.Add(0);
+                }
+                else
+                {
+                    _calories.Add(Int32.Parse(line));
+                }
+            }
+            _calories.Add(0);
+        }
+        public void partOne()
+        {
+            int currentElfCalories = new int();
+            int highestCalories = new int();
+
+            _calories.ForEach(i =>
+            {
+                if (i == 0)
+                {
+                    if (currentElfCalories > highestCalories)
+                    {
+                        highestCalories = currentElfCalories;
+                    }
+                    currentElfCalories = 0;
+                }
+                else
+                {
+                    currentElfCalories += i;
+                }
+            });
+            Console.WriteLine("The awnser to part 1 of day 1 = " + highestCalories);
+        }
+
+        public void partTwo()
+        {
+            int currentElfCalories = new int();
+            List<int> elfCalories = new List<int>();
+
+            _calories.ForEach(i =>
+            {
+                if (i == 0)
+                {
+                    elfCalories.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                }
+                else
+                {
+                    currentElfCalories += i;
+                }
+            });
+            elfCalories.Sort((x, y) => y.CompareTo(x));
+            int combinedCalories = elfCalories[0] + elfCalories[1] + elfCalories[2];
+            Console.WriteLine("The awnser to part 2 of day 1 = " + combinedCalories);
+        }
+    }
+}
--- Advent of Code 2022/Advent of Code 2022/Program.cs
+++ Advent of Code 2022/Advent of Code 2022/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace Advent_of_Code
+﻿using Advent_of_Code_2022.Days;
+
+namespace Advent_of_Code
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Day1 day1 = new Day1();
             Console.WriteLine("*******Day 1 solution*******");
-
+            day1.partOne();
+            day1.partTwo();
 
             Console.WriteLine("----------------------------");
 
