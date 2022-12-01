fe260ec6131c276b93d7e9e502c28c6d6039ca8a
wvm28 <wouter.van.mierlo@sogeti.com>
2022-12-01 10:37:09 +0100

    setup project

---
+++ Advent of Code 2022/Advent of Code 2022/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Advent_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*******Day 1 solution*******");
+
+
+            Console.WriteLine("----------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
