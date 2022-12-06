3f7f9589cc817b81f57fa01975533b39596a733d
wvm28 <wouter.van.mierlo@sogeti.com>
2022-12-06 09:58:05 +0100

    finished day 6

---
+++ Advent of Code 2022/Advent of Code 2022/Days/Day06.cs
@@ -0,0 +1,47 @@
+﻿namespace Advent_of_Code_2022.Days
+{
+    class Day06 : IDay
+    {
+        private string _dataStream = "";
+        public Day06()
+        {
+            _dataStream = File.ReadLines(@"Days\Input\Day06.txt").ToList()[0];
+        }
+
+        public void partOne()
+        {
+            Console.WriteLine(getLastIndexWithoutDuplicates(3));
+        }
+
+        public void partTwo()
+        {
+            Console.WriteLine(getLastIndexWithoutDuplicates(13));
+        }
+
+        public int getLastIndexWithoutDuplicates(int amount)
+        {
+            Queue<char> previousChars = new Queue<char>();
+            int firstMarkerStart = 0;
+            for (int i = 0; i < _dataStream.Length; i++)
+            {
+                if (i < amount)
+                {
+                    previousChars.Enqueue(_dataStream[i]);
+                }
+                else
+                {
+                    previousChars.Enqueue(_dataStream[i]);
+                    if (previousChars.GroupBy(value => value).Any(@group => @group.Count() > 1))
+                    {
+                    }
+                    else
+                    {
+                        if (firstMarkerStart == 0) firstMarkerStart = (i + 1);
+                    };
+                    previousChars.Dequeue();
+                }
+            }
+            return firstMarkerStart;
+        }
+    }
+}
